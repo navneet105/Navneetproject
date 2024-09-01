3e8dade55f349b3a5f59ebae4112f4c33c24a62a
Navneet Verma <vnavneet105@gmail.com>
2024-09-01 18:36:32 +0530

    tree

---
+++ NAV/src/Lec30/Tree.java
@@ -0,0 +1,14 @@
+package Lec30;
+
+public class Tree {
+
+	public static void main(String[] args) {
+		// hirerarical data structure  before that we have read all linear data structure
+		//generic tree binery tree binery search tree prefix tree
+		//leaf node jiske ek bhi chile nhi h
+		//main parent is known as root(node)
+		//binery tree binery search at most 2 child minimum 0
+
+	}
+
+}
