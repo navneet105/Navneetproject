ae46f38c73e3308acd79702bdf1aadc88ae1804e
Navneet Verma <vnavneet105@gmail.com>
2024-09-01 17:00:50 +0530

    ll
    ll

--- NAV/src/Lec26/Next_GREATER_NUMBER.java
+++ NAV/src/Lec26/Next_GREATER_NUMBER.java
@@ -1,9 +1,31 @@
 package Lec26;
 
+import java.util.Scanner;
+
 public class Next_GREATER_NUMBER {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        while (t-- > 0) {
+            int n = scanner.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = scanner.nextInt();
+            }
+            for (int i = 0; i < n; i++) {
+                int j;
+                for (j = i + 1; j < n; j++) {
+                    if (arr[j] > arr[i]) {
+                        System.out.print(arr[i] + "," + arr[j] + " ");
+                        break;
+                    }
+                }
+                if (j == n) {
+                    System.out.print(arr[i] + ",-1 ");
+                }
+            }
+            System.out.println();}
 
 	}
 
---
+++ NAV/src/Lec29/java_LinkedList.java
@@ -0,0 +1,25 @@
+package Lec29;
+
+import java.util.LinkedList;
+
+public class java_LinkedList {
+
+	public static void main(String[] args) {
+		LinkedList<Integer>ll=new LinkedList<>();
+		ll.add(10);
+		ll.add(20);
+		ll.add(1,-9);
+		
+		System.out.println(ll);
+		for(int val:ll) {
+			System.out.print(val+" ");
+		}
+		System.out.println();
+		System.out.println(ll.get(1));
+		System.out.println(ll.remove());
+	//	System.out.println(ll.removeFirst());
+	//	System.out.println(ll.remove(0));
+
+	}
+
+}
